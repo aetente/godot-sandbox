b243b36b7b04fcde2b4ce3fce0f58d6b5ab98d2b
Yehoshua Kopirin <aetente@gmail.com>
2023-11-20 21:29:52 +0200

    feat: rotating person with camera

--- scripts/person_balance.gd
+++ scripts/person_balance.gd
@@ -12,50 +12,62 @@ var F = 340
 var time_start = 0
 var time_now = 0
 
-
-@onready var leftLeg = get_node("rigids/leftFoot")
-@onready var rightLeg = get_node("rigids/rightFoot")
-
 @onready var base = $"rigids"
 @onready var body = $"rigids/torsoEnd/torso"
-@onready var bodyControll = $BodyControll
+@onready var head = $"rigids/torsoEnd/torso/head"
 @onready var torsoEnd = $"rigids/torsoEnd"
+
 @onready var leftFoot = $"rigids/leftFoot/leftFootEnd"
 @onready var rightFoot = $"rigids/rightFoot/rightFootEnd"
+@onready var leftLeg = $"rigids/leftFoot"
+@onready var rightLeg = $"rigids/rightFoot"
 
-@onready var mainJoint = get_node("torsoJoint")
+@onready var leftArm = $"rigids/torsoEnd/torso/leftArm"
+@onready var rightArm = $"rigids/torsoEnd/torso/rightArm"
+@onready var leftHandEnd = $"rigids/torsoEnd/torso/leftHandEnd"
+@onready var rightHandEnd = $"rigids/torsoEnd/torso/rightHandEnd"
+
+
+@onready var cameraPivot = $"CameraPivot"
 
 var walkForce = 0.1
 var walkAnimationTimer = 0
 var isWalking = false
 
-var leftFootStartPos = Vector3(0,0,0)
-var rightFootStartPos = Vector3(0,0,0)
-
 var leftLegDesiredAngle = Vector3(0,0,0)
 var rightLegDesiredAngle = Vector3(0,0,0)
+var torsoEndDesiredAngle = Vector3(0,0,0)
+
+var previousCameraPivotAngle = Vector3(0,0,0)
+var cameraPivotAngleDifference = Vector3(0,0,0)
 
 var movedRightLeg = false
 var movedLeftLeg = false
 
 var movingDirection = Vector2(0,0)
 
+var mouse_sensitivity = 0.3
+var max_angle_forward = 1.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	leftFootStartPos = leftFoot.position
-	rightFootStartPos = rightFoot.position
+	
 	leftLegDesiredAngle = leftLeg.desired_angle
 	rightLegDesiredAngle = rightLeg.desired_angle
+	torsoEndDesiredAngle = torsoEnd.desired_angle
+	
+	previousCameraPivotAngle = cameraPivot.rotation
 	time_start = Time.get_unix_time_from_system()
 	
 func animateWalk():
-	walkAnimationTimer += 0.1
+	walkAnimationTimer += 0.2
+	torsoEnd.desired_angle.z = -abs(movingDirection.x) / 2
 	if (sin(walkAnimationTimer) > 0 && !movedRightLeg):
 		#leftLeg.rotation.z = sin(walkAnimationTimer) * 1
 		#rightLeg.rotation.z = -sin(walkAnimationTimer) * 0.1
 		movedRightLeg = true
 		movedLeftLeg = false
-		leftLeg.desired_angle.z = 1 * movingDirection.x
+		leftLeg.desired_angle.z = max_angle_forward * movingDirection.x
 		rightLeg.desired_angle.z = 0 * movingDirection.x
 		pass
 	elif (sin(walkAnimationTimer) <= 0 && !movedLeftLeg):
@@ -64,12 +76,44 @@ func animateWalk():
 		movedRightLeg = false
 		movedLeftLeg = true
 		leftLeg.desired_angle.z = 0 * movingDirection.x
-		rightLeg.desired_angle.z = 1 * movingDirection.x
+		rightLeg.desired_angle.z = max_angle_forward * movingDirection.x
 		pass
 	#leftFoot.position.y += (sin(walkAnimationTimer) + 1) / 20 * 1
 	#leftFoot.position.x += (cos(walkAnimationTimer)) / 20 * 1
 	#rightFoot.position.y += -(sin(walkAnimationTimer) + 1) / 20 * 1
 	#rightFoot.position.x += -(cos(walkAnimationTimer)) / 20 * 1
+	
+func handleRotation():
+	#torsoEnd.rotation.y = cameraPivot.rotation.y
+	cameraPivotAngleDifference = cameraPivot.rotation - previousCameraPivotAngle
+	
+	leftLegDesiredAngle.y += cameraPivotAngleDifference.y
+	rightLegDesiredAngle.y += cameraPivotAngleDifference.y
+	torsoEndDesiredAngle.y += cameraPivotAngleDifference.y
+	
+	#base.rotation.y += cameraPivotAngleDifference.y
+	#head.desired_angle.y += cameraPivotAngleDifference.y
+	
+	body.desired_angle.y += cameraPivotAngleDifference.y
+	torsoEnd.desired_angle.y += cameraPivotAngleDifference.y
+	leftLeg.desired_angle.y += cameraPivotAngleDifference.y
+	rightLeg.desired_angle.y += cameraPivotAngleDifference.y
+	leftFoot.desired_angle.y += cameraPivotAngleDifference.y
+	rightFoot.desired_angle.y += cameraPivotAngleDifference.y
+	
+	#leftArm.desired_angle.y += cameraPivotAngleDifference.y
+	#rightArm.desired_angle.y += cameraPivotAngleDifference.y
+	#leftHandEnd.desired_angle.y += cameraPivotAngleDifference.y
+	#rightHandEnd.desired_angle.y += cameraPivotAngleDifference.y
+	
+	previousCameraPivotAngle = cameraPivot.rotation
+	pass
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		cameraPivot.rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		cameraPivot.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		cameraPivot.rotation_degrees.x = clamp(cameraPivot.rotation_degrees.x, -90, 90)
 
 func handleWalk():
 	isWalking = false
@@ -79,22 +123,22 @@ func handleWalk():
 	if Input.is_action_pressed("KEY_W"):
 		movingDirection.x += 1
 		#torsoEnd.desired_angle.z = -PI / 4
-		body.apply_central_impulse(-bodyControll.transform.basis.z*walkForce)
+		body.apply_central_impulse(-body.transform.basis.y*walkForce)
 		isWalking = true
 		
 	if Input.is_action_pressed("KEY_A"):
 		movingDirection.y -= 1
-		body.apply_central_impulse(-bodyControll.transform.basis.x*walkForce * 10)
+		body.apply_central_impulse(-body.transform.basis.z*walkForce * 10)
 		isWalking = true
 		
 	if Input.is_action_pressed("KEY_D"):
 		movingDirection.y += 1
-		body.apply_central_impulse(bodyControll.transform.basis.x*walkForce * 10)
+		body.apply_central_impulse(body.transform.basis.z*walkForce * 10)
 		isWalking = true
 		
 	if Input.is_action_pressed("KEY_S"):
 		movingDirection.x -= 1
-		body.apply_central_impulse(bodyControll.transform.basis.z*walkForce)
+		body.apply_central_impulse(body.transform.basis.y*walkForce)
 		isWalking = true
 		
 	if isWalking:
@@ -107,10 +151,9 @@ func handleWalk():
 		movingDirection = Vector2(0,0)
 		leftLeg.desired_angle = leftLegDesiredAngle 
 		rightLeg.desired_angle = rightLegDesiredAngle
+		torsoEnd.desired_angle = torsoEndDesiredAngle 
 		#leftLeg.rotation.z = 0
 		#rightLeg.rotation.z = 0
-		#leftFoot.position = leftFootStartPos
-		#rightFoot.position = rightFootStartPos
 		
 func handleBalance(ray):
 	if ray.is_colliding():
@@ -128,6 +171,9 @@ func handleBalance(ray):
 func _process(delta):
 	time_now = Time.get_unix_time_from_system()
 	var r = time_now - time_start
+	
+	cameraPivot.global_transform.origin = head.global_transform.origin
+	handleRotation()
 	handleWalk()
 	
 	
--- scripts/test_balance_stick.gd
+++ scripts/test_balance_stick.gd
@@ -1,5 +1,6 @@
 extends RigidBody3D
 
+@export var enable = true
 @export var is_global_rotation = false
 
 @export var desired_angle: Vector3 = Vector3(0,0,0)
@@ -20,6 +21,9 @@ extends RigidBody3D
 @export var linear_spring_damping: float = 20.0
 @export var max_linear_force: float = 500.0
 
+
+@export var debug = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -30,25 +34,34 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	
-	if (apply_for_position):
-		var position_difference:Vector3 = desired_position - transform.origin
+	if (enable):
+		if (apply_for_position):
+			var position_difference:Vector3 = desired_position - transform.origin
+			
+			if position_difference.length_squared() > 1.0:
+				transform.origin = desired_position
+			else:
+				var force: Vector3 = hookes_law(position_difference, self.linear_velocity, linear_spring_stiffness, linear_spring_damping)
+				force = force.limit_length(max_linear_force)
+				self.linear_velocity += (force * delta)
 		
-		if position_difference.length_squared() > 1.0:
-			transform.origin = desired_position
-		else:
-			var force: Vector3 = hookes_law(position_difference, self.linear_velocity, linear_spring_stiffness, linear_spring_damping)
-			force = force.limit_length(max_linear_force)
-			self.linear_velocity += (force * delta)
-	
-	var current_rotation = rotation
-	if (is_global_rotation):
-		current_rotation = global_transform.basis.get_euler()
-	var rotation_difference: Vector3 = (desired_angle - current_rotation)
-	if (abs(rotation_difference.length()) > angular_error_threshold):
-		var torque = hookes_law(rotation_difference, self.angular_velocity, angular_spring_stiffness, angular_spring_damping)
-		torque = torque.limit_length(max_angular_force)
-		self.angular_velocity += torque * delta
+		var current_basis = transform.basis
+		if (is_global_rotation):
+			current_basis = global_transform.basis
+		var desired_basis = Basis.from_euler(desired_angle)
+		var rotation_difference: Vector3 = (desired_basis * current_basis.inverse()).get_euler()
+		if (abs(rotation_difference.length()) > angular_error_threshold):
+			
+			var torque = hookes_law(rotation_difference, self.angular_velocity, angular_spring_stiffness, angular_spring_damping)
+			torque = torque.limit_length(max_angular_force)
+			if (debug):
+				print("desired_angle: ", desired_angle)
+				print("current_rotation: ", current_basis.get_euler())
+				#print("rotation_difference: ", rotation_difference)
+				#print("rotation_difference.length(): ", rotation_difference.length())
+				print("====================")
+				#print("torque: ", torque)
+			self.angular_velocity += torque * delta
 
 
 func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, damping: float) -> Vector3:
