c48e4f0a324f29acf235b3c5972816cc2620ad08
Yehoshua Kopirin <aetente@gmail.com>
2024-01-02 16:53:35 +0200

    feat: some new stuff for trees generation script

--- scripts/fill_trees_test1.gd
+++ scripts/fill_trees_test1.gd
@@ -4,6 +4,16 @@ extends Node3D
 
 @export var tree : PackedScene
 @export var amount_of_trees : int = 100
+@export var tree_distance_x : float = 1.0
+@export var tree_frequency_x : float = 54365421
+@export var tree_distance_z : float = 1.0
+@export var tree_frequency_z : float = 23452345
+@export var tree_distance_y : float = 0.0
+@export var tree_frequency_y : float = 100.0
+
+@export var tree_max_scale : float = 1.0
+@export var tree_min_scale : float = 1.0
+@export var tree_scale_frequency : float = 100.0
 
 func create_instance(add):
 	var scene_instance = add.instantiate()
@@ -14,9 +24,12 @@ func _ready():
 	for i in range(amount_of_trees):
 		var tree_element
 		tree_element = create_instance(tree)
-		tree_element.position.x = sin(i * 342513134)
-		tree_element.position.z = cos(i * 23452345)
+		tree_element.position.x = tree_distance_x * sin(i * tree_frequency_x)
+		tree_element.position.z = tree_distance_z * cos(i * tree_frequency_z)
+		tree_element.position.y = tree_distance_y * sin(i * tree_frequency_y)
 		tree_element.rotation.y = 360 * cos(i * 23452345)
+		var tree_scale_value = (tree_max_scale - tree_min_scale) * (sin(tree_scale_frequency * i) + 1) / 2 + tree_min_scale
+		tree_element.scale = Vector3(tree_scale_value,tree_scale_value,tree_scale_value)
 		add_child(tree_element)
 
 
