9439bb751925bd8e62d96da6f79b5ee795a9bf34
Yehoshua Kopirin <aetente@gmail.com>
2024-11-19 22:32:22 +0200

    feat: timer to stop the note after a while

--- scripts/rigid_midi_part.gd
+++ scripts/rigid_midi_part.gd
@@ -1,5 +1,8 @@
 extends RigidBody3D
 
+
+@onready var timer = Timer.new()
+
 @export var audio_velocity_threshold = 0.5
 
 var rng = RandomNumberGenerator.new()
@@ -17,11 +20,19 @@ var is_playing = false
 
 var midi_out = MidiOut.new()
 
-
 var chord_to_note = preload("res://scripts/chord_to_note.gd")
+
+var note_to_stop = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	timer.one_shot = true
+	timer.wait_time = 0.3
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+
+
 	chord_to_note = chord_to_note.new()
 	midi_out.open_port(midi_port)
 	contact_monitor = true
@@ -29,11 +40,13 @@ func _ready():
 	body_entered.connect(_body_entered)
 	pass # Replace with function body.
 
+	
+func _on_timer_timeout():
+	midi_out.send_message([0x80, note_to_stop, 127])
+
 # func fade_in():
 # 	tween.tween_property(audio_streamer, "volume_db", audio_default_volume_db, transition_duration).set_trans(transition_type)
 
-func _on_audio_streamer_finished():
-	is_playing = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -44,12 +57,15 @@ func lerp(a, b, t):
 
 func _body_entered(body:Node):
 	if (linear_velocity.length() > audio_velocity_threshold):
+		timer.stop()
 		if is_debug:
 			print("sound")
 		# turn off
 		var new_note = chord_to_note.chord_to_note()
-		midi_out.send_message([0x80, 72, 127])
+		note_to_stop = new_note
+		midi_out.send_message([0x80, note_to_stop, 127])
 		# play sound
 		midi_out.send_message([0x90, new_note, 127])
 		midi_out.send_message([0xB0, 10, randi_range(0, 127)])
+		timer.start()
 	pass # Replace with function body.
